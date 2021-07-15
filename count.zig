658a0a1aef2e8398fa6486ac5037abf84fbb6efe
Tom <tom@contextfree.info>
2021-07-15 08:36:04 -0700

    Stack allocate and format

--- count.zig
+++ count.zig
@@ -11,32 +11,31 @@ fn count(n: usize, interval: f64) void {
     while (i < n) : (i += 1) {
         Loop.instance.?.sleep(wait_ns);
         const thread_id = std.Thread.getCurrentId();
-        std.debug.print(
-            "{} {}: {} seconds\n", .{thread_id, time_s(), interval}
-        );
+        std.debug.print("{} {}: {} seconds\n", .{ thread_id, time_s(), interval });
     }
 }
 
 fn time_s() f64 {
-    return
-        @intToFloat(f64, std.time.nanoTimestamp()) /
+    return @intToFloat(f64, std.time.nanoTimestamp()) /
         @intToFloat(f64, std.time.ns_per_s);
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = &arena.allocator;
-
+    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer arena.deinit();
+    // const allocator = &arena.allocator;
     const thread_id = std.Thread.getCurrentId();
-    std.debug.print("{} {}: begin\n", .{thread_id, time_s()});
+    std.debug.print("{} {}: begin\n", .{ thread_id, time_s() });
 
-    const frames = try allocator.alloc(@Frame(count), 2);
-    // defer allocator.free(frames);
-    frames[0] = async count(2, 1.0);
-    frames[1] = async count(3, 0.6);
+    // const frames = try allocator.alloc(@Frame(count), 2);
+    // Not needed for arena: // defer allocator.free(frames);
+    const frames = [_]@Frame(count){
+        async count(2, 1.0),
+        async count(3, 0.6),
+    };
 
     for (frames) |*frame| {
-        await frame;
+        // await frame;
+        await @ptrCast(anyframe->void, frame);
     }
 }
