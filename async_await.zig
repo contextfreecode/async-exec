bebb84e7adbcbc154beff9a77a0ed1feaafb844f
Tom <tom@contextfree.info>
2021-07-17 05:28:45 -0700

    Add mini zig event loop examples

---
+++ async_await.zig
@@ -0,0 +1,67 @@
+// Async Await in 60 LOC by MasterQ32
+// https://gist.github.com/MasterQ32/ff534f912c5faedbbb57974317e79778
+
+const std = @import("std");
+
+// usage:
+fn asyncMain() !void
+{
+    // Start two interleaving tasks
+    var task_a = async waitUntilAndPrint(start + 1000, start + 1200, "task a");
+    var task_b = async waitUntilAndPrint(start + 500,  start + 1300, "task b");
+
+    await task_a;
+    await task_b;
+}
+
+fn waitUntilAndPrint(time1: i64, time2: i64, name: []const u8) void 
+{
+    waitForTime(time1);
+    std.log.info("[{s}] it is now {} ms since start!", .{ name, std.time.milliTimestamp() - start });
+
+    waitForTime(time2);
+    std.log.info("[{s}] it is now {} ms since start!", .{ name, std.time.milliTimestamp() - start });
+}
+
+// impementation
+const Task = struct { frame: anyframe, time: i64 };
+var task_list = [1]?Task { null } ** 10;
+var start: i64 = 0;
+
+fn waitForTime(time: i64) void {
+    suspend {
+        // append the task into a list structure "tasks to be resumed later" and store the @frame()
+        // as well as a condition (in this case: time)
+        const slot = for(task_list) |*task| { if(task.* == null) break task; } else unreachable;
+        slot.* = Task { .frame = @frame(), .time = time };
+    }
+}
+
+pub fn main() !void
+{
+    start = std.time.milliTimestamp();
+
+    var main_task = async asyncMain();
+
+    while(true) // this is "the event loop"
+    {
+        const now = std.time.milliTimestamp();
+
+        var any = false; // store if we have any tasks left
+        for(task_list) |*task| { 
+            if(task.* != null) {
+                any = true;
+                if(task.*.?.time <= now) { // resume condition
+                    var frame = task.*.?.frame; // resume location
+                    task.* = null; // make task slot available again
+                    resume frame;
+                }
+            }
+        }
+        if(!any) break; // all tasks done, we can now exit the program
+    }
+
+    // finalize the tasks properly (we know they finished as they are no more tasks left)
+    // but main might have returned a error!
+    try nosuspend await main_task;
+}
---
+++ fib.zig
@@ -0,0 +1,84 @@
+// Small event loop by kprotty
+// https://gist.github.com/kprotty/e4409f83f3e6ac87ae1c60f7aaaae95e
+
+const std = @import("std");
+const allocator = std.heap.page_allocator;
+
+pub fn main() !void {
+    const n = 10;
+    const ret = try (try Task.run(fib, .{n}));
+    std.debug.warn("fib({}) = {}\n", .{n, ret});
+}
+
+fn fib(n: usize) std.mem.Allocator.Error!usize {
+    Task.yield();
+
+    if (n <= 1)
+        return n;
+    
+    const l = try allocator.create(@Frame(fib));
+    defer allocator.destroy(l);
+    const r = try allocator.create(@Frame(fib));
+    defer allocator.destroy(r);
+
+    l.* = async fib(n - 1);
+    r.* = async fib(n - 2);
+
+    const lv = await l;
+    const rv = await r;
+
+    return (try lv) + (try rv);
+}
+
+pub const Task = struct {
+    next: ?*Task = undefined,
+    frame: anyframe,
+
+    fn ReturnTypeOf(comptime func: anytype) type {
+        return @typeInfo(@TypeOf(func)).Fn.return_type orelse unreachable;
+    }
+
+    pub fn run(comptime asyncFn: anytype, args: anytype) !ReturnTypeOf(asyncFn) {
+        const Wrapper = struct {
+            fn entry(fn_args: anytype, task: *Task, res: *?ReturnTypeOf(asyncFn)) void {
+                Task.yield();
+                const result = @call(.{}, asyncFn, fn_args);
+                suspend res.* = result;
+            }
+        };
+
+        var task: Task = undefined;
+        var result: ?ReturnTypeOf(asyncFn) = null;
+        var frame = async Wrapper.entry(args, &task, &result);
+
+        while (Task.find()) |runnable_task|
+            resume runnable_task.frame;
+
+        return result orelse error.DeadLocked;        
+    }
+
+    var head: ?*Task = null;
+    var tail: ?*Task = null;
+
+    pub fn yield() void {
+        var task = Task{ .frame = @frame() };
+        suspend task.schedule();
+    }
+
+    pub fn schedule(self: *Task) void {
+        self.next = null;
+        if (tail) |t|
+            t.next = self;
+        if (head == null)
+            head = self;
+        tail = self;
+    }
+
+    fn find() ?*Task {
+        const task = head orelse return null;
+        head = task.next;
+        if (head == null)
+            tail = null;
+        return task;
+    }
+};
