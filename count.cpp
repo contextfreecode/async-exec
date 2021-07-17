feb5ba6d7e93370e67f88acaabb47f493eb24c2e
Tom <tom@contextfree.info>
2021-07-17 09:50:10 -0700

    Start kuro

--- count.cpp
+++ count.cpp
@@ -1,6 +1,13 @@
+#include <chrono>
 #include <iostream>
 #include <kuro/kuro.hpp>
 
+auto run() -> kuro::task<void> {
+    std::cout << "begin" << std::endl;
+    co_await kuro::sleep_for(0.6);
+    std::cout << "end" << std::endl;
+}
+
 auto main() -> int {
-  std::cout << "Hi!\n";
+    kuro::event_loop::run(run());
 }
