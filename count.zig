5f3424426b1b719cde70bc42248bf12c3215a810
Tom <tom@contextfree.info>
2021-07-17 16:54:58 -0700

    Prep zig for alternate

--- count.zig
+++ count.zig
@@ -3,14 +3,15 @@
 pub const io_mode = .evented;
 
 const std = @import("std");
-const Loop = std.event.Loop;
+// See: https://github.com/ziglang/zig/blob/85755c51d529e7d9b406c6bdf69ce0a0f33f3353/lib/std/event/loop.zig#L765
+const sleep = std.event.Loop.instance.?.sleep;
 
 fn count(n: usize, interval: f64) void {
     std.debug.print("{} {}: before loop\n", .{ std.Thread.getCurrentId(), time_s() });
     var i: usize = 0;
     const wait_ns = @floatToInt(u64, interval * std.time.ns_per_s);
     while (i < n) : (i += 1) {
-        Loop.instance.?.sleep(wait_ns);
+        sleep(wait_ns);
         const thread_id = std.Thread.getCurrentId();
         std.debug.print("{} {}: {} seconds\n", .{ thread_id, time_s(), interval });
     }
@@ -21,23 +22,20 @@ fn time_s() f64 {
         @intToFloat(f64, std.time.ns_per_s);
 }
 
-pub fn main() !void {
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
-    // const allocator = &arena.allocator;
+pub fn run() void {
     const thread_id = std.Thread.getCurrentId();
     std.debug.print("{} {}: begin\n", .{ thread_id, time_s() });
-
-    // const frames = try allocator.alloc(@Frame(count), 2);
-    // Not needed for arena: // defer allocator.free(frames);
     var frames = [_]@Frame(count){
         async count(2, 1.0),
         async count(3, 0.6),
     };
     std.debug.print("frame size: {}\n", .{@sizeOf(@TypeOf(frames[0]))});
-
     for (frames) |*frame| {
         await frame;
     }
     std.debug.print("{} {}: done\n", .{ thread_id, time_s() });
+}
+
+pub fn main() void {
+    run();
 }
