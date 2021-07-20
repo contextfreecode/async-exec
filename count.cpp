ff19d3884705cd4c1e9b3a0340c19bc611eb550a
Tom <tom@contextfree.info>
2021-07-20 08:04:31 -0700

    Work more on cpp count

--- count.cpp
+++ count.cpp
@@ -1,10 +1,20 @@
 #include <chrono>
 #include <iostream>
 #include <kuro/kuro.hpp>
+#include <vector>
+
+auto count(size_t n, double interval) -> kuro::task<void> {
+    for (size_t i = 0; i < n; i += 1) {
+        co_await kuro::sleep_for(interval);
+        std::cout << interval << " seconds" << std::endl;
+    }
+}
 
 auto run() -> kuro::task<void> {
     std::cout << "begin" << std::endl;
-    co_await kuro::sleep_for(0.6);
+    auto frame1 = count(2, 1.0);
+    auto frame2 = count(3, 0.6);
+    co_await kuro::gather(frame1, frame2);
     std::cout << "end" << std::endl;
 }
 
