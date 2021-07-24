7be19b5862a4cb3142c22145e4e6a809b2c35840
Tom <tom@contextfree.info>
2021-07-23 23:29:52 -0700

    Format more

--- count.cpp
+++ count.cpp
@@ -4,21 +4,19 @@
 #include <vector>
 
 auto count(size_t n, double interval) -> kuro::task<void> {
-    for (size_t i = 0; i < n; i += 1) {
-        co_await kuro::sleep_for(interval);
-        std::cout << interval << " seconds" << std::endl;
-    }
+  for (size_t i = 0; i < n; i += 1) {
+    co_await kuro::sleep_for(interval);
+    std::cout << interval << " seconds" << std::endl;
+  }
 }
 
 auto run() -> kuro::task<void> {
-    std::cout << "begin" << std::endl;
-    auto frame1 = count(2, 1.0);
-    auto frame2 = count(3, 0.6);
-    std::cout << "size " << sizeof(frame1) << std::endl;
-    co_await kuro::gather(frame1, frame2);
-    std::cout << "end" << std::endl;
+  std::cout << "begin" << std::endl;
+  auto frame1 = count(2, 1.0);
+  auto frame2 = count(3, 0.6);
+  std::cout << "size " << sizeof(frame1) << std::endl;
+  co_await kuro::gather(frame1, frame2);
+  std::cout << "end" << std::endl;
 }
 
-auto main() -> int {
-    kuro::event_loop::run(run());
-}
+auto main() -> int { kuro::event_loop::run(run()); }
