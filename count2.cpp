f32d9c3645bcce1a0c489d07dc3ac8355fa0811b
Tom <tom@contextfree.info>
2021-07-23 23:27:02 -0700

    Get closer

--- count2.cpp
+++ count2.cpp
@@ -7,9 +7,12 @@
 
 template<typename Scheduler>
 requires unifex::scheduler<Scheduler>
-auto count(Scheduler& scheduler) -> unifex::task<void> {
-    using namespace std::chrono_literals;
-    co_await scheduler.schedule_at(unifex::now(scheduler) + 1000ms);
+auto count(Scheduler& scheduler, int n, double interval) -> unifex::task<void> {
+    auto duration = std::chrono::duration<double>(1.0);
+    for (size_t i = 0; i < n; i += 1) {
+        co_await scheduler.schedule_at(unifex::now(scheduler) + duration);
+        std::cout << interval << " seconds" << std::endl;
+    }
     std::cout << "done" << std::endl;
 }
 
@@ -17,15 +20,15 @@ auto main() -> int {
     // using namespace std::chrono_literals;
     // unifex::linuxos::io_epoll_context ctx;
     auto ctx = unifex::linuxos::io_epoll_context{};
-    unifex::inplace_stop_source stopSource;
-    std::thread t{[&] { ctx.run(stopSource.get_token()); }};
-    unifex::scope_guard stopOnExit = [&]() noexcept {
-        stopSource.request_stop();
-        t.join();
+    unifex::inplace_stop_source stop_source;
+    std::thread thread{[&] { ctx.run(stop_source.get_token()); }};
+    unifex::scope_guard stop_on_exit = [&]() noexcept {
+        stop_source.request_stop();
+        thread.join();
     };
     auto scheduler = ctx.get_scheduler();
     // auto task = scheduler.schedule_at(unifex::now(scheduler) + 100ms);
-    auto task = count(scheduler);
+    auto task = count(scheduler, 2, 1.0);
     std::cout << sizeof(task) << std::endl;
     unifex::sync_wait(std::move(task));
     std::cout << "really done" << std::endl;
