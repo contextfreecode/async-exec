1b72088c69d549f322b56d4966754bdb16796faf
Tom <tom@contextfree.info>
2021-07-23 23:17:53 -0700

    Maybe get unifex working

--- count2.cpp
+++ count2.cpp
@@ -9,14 +9,24 @@ template<typename Scheduler>
 requires unifex::scheduler<Scheduler>
 auto count(Scheduler& scheduler) -> unifex::task<void> {
     using namespace std::chrono_literals;
-    co_await scheduler.schedule_at(unifex::now(scheduler) + 100ms);
+    co_await scheduler.schedule_at(unifex::now(scheduler) + 1000ms);
     std::cout << "done" << std::endl;
 }
 
 auto main() -> int {
+    // using namespace std::chrono_literals;
+    // unifex::linuxos::io_epoll_context ctx;
     auto ctx = unifex::linuxos::io_epoll_context{};
+    unifex::inplace_stop_source stopSource;
+    std::thread t{[&] { ctx.run(stopSource.get_token()); }};
+    unifex::scope_guard stopOnExit = [&]() noexcept {
+        stopSource.request_stop();
+        t.join();
+    };
     auto scheduler = ctx.get_scheduler();
+    // auto task = scheduler.schedule_at(unifex::now(scheduler) + 100ms);
     auto task = count(scheduler);
     std::cout << sizeof(task) << std::endl;
     unifex::sync_wait(std::move(task));
+    std::cout << "really done" << std::endl;
 }
