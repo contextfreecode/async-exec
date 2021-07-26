ea30ab6d7209411b468aa798268765e52d2e377e
Tom <tom@contextfree.info>
2021-07-26 06:30:20 -0700

    Prep threaded

--- exec.zig
+++ exec.zig
@@ -2,31 +2,41 @@ const std = @import("std");
 
 const Task = struct { frame: anyframe, time: i128 };
 var task_list = [1]?Task{null} ** 10;
+var task_mutex = std.Thread.Mutex{};
 
 pub fn runTasks() void {
     while (true) {
+        const lock = task_mutex.acquire();
         const now = std.time.nanoTimestamp();
         var any = false;
-        for (task_list) |*task| {
+        var frame = for (task_list) |*task| {
             if (task.* != null) {
                 any = true;
                 if (task.*.?.time <= now) {
                     var frame = task.*.?.frame;
                     task.* = null;
-                    resume frame;
+                    break frame;
                 }
             }
-        }
+        } else null;
+        lock.release();
+        if (frame != null) resume frame.?;
         if (!any) break;
     }
 }
 
 pub fn sleep(ns: u64) void {
     suspend {
-        const slot = for (task_list) |*task| {
-            if (task.* == null) break task;
-        } else unreachable;
+        const lock = task_mutex.acquire();
+        defer lock.release();
+        const slot = findSlot();
         const time = std.time.nanoTimestamp() + ns;
         slot.* = Task{ .frame = @frame(), .time = time };
     }
+}
+
+fn findSlot() *?Task {
+    return for (task_list) |*task| {
+        if (task.* == null) break task;
+    } else unreachable;
 }
