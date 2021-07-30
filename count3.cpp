23cd1f9763af6b6bf75e9c9dbf586a94be4f0032
Tom <tom@contextfree.info>
2021-07-29 22:01:04 -0700

    Get closer

--- count3.cpp
+++ count3.cpp
@@ -1,11 +1,20 @@
 #include <coroutine>
+#include <iostream>
 
 template <typename Value>
 struct Task {
   struct promise_type {
-    //
+    auto get_return_object() { return value; }
+    auto final_suspend() noexcept -> std::suspend_never { return {}; }
+    auto initial_suspend() -> std::suspend_never { return {}; }
+    auto return_value(Value value) {
+      this->value = value;
+    }
+    auto unhandled_exception() {}
+    Value value;
   };
-  //
+
+  std::coroutine_handle<promise_type> handle;
 };
 
 auto thing() -> Task<int> {
@@ -15,4 +24,5 @@ auto thing() -> Task<int> {
 auto main() -> int {
   // auto hi = co_await thing();
   auto hi = thing();
+  std::cout << hi.handle.promise().get_return_object() << "\n";
 }
