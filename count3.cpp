3accdcff9ed4a9267731cddcab73ebebc2de93b8
Tom <tom@contextfree.info>
2021-07-29 22:21:05 -0700

    Get it running

--- count3.cpp
+++ count3.cpp
@@ -4,7 +4,9 @@
 template <typename Value>
 struct Task {
   struct promise_type {
-    auto get_return_object() { return value; }
+    auto get_return_object() {
+      return std::coroutine_handle<promise_type>::from_promise(*this);
+    }
     auto final_suspend() noexcept -> std::suspend_never { return {}; }
     auto initial_suspend() -> std::suspend_never { return {}; }
     auto return_value(Value value) {
@@ -24,5 +26,5 @@ auto thing() -> Task<int> {
 auto main() -> int {
   // auto hi = co_await thing();
   auto hi = thing();
-  std::cout << hi.handle.promise().get_return_object() << "\n";
+  std::cout << hi.handle.promise().value << "\n";
 }
