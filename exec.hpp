9ef1d5e17b57b826963287d62d5506f576abf42d
Tom <tom@contextfree.info>
2021-08-04 19:36:09 -0700

    Work toward awaitable tasks

--- exec.hpp
+++ exec.hpp
@@ -25,12 +25,26 @@ template <typename Value>
 struct Task {
   struct promise_type {
     Value value;
+    std::coroutine_handle<> parent;
 
     auto get_return_object() -> std::coroutine_handle<promise_type> {
       return std::coroutine_handle<promise_type>::from_promise(*this);
     }
 
-    auto final_suspend() noexcept -> std::suspend_never { std::cout << "final" << std::endl; return {}; }
+    auto final_suspend() noexcept {
+      struct Awaitable {
+        auto await_ready() noexcept -> bool { return false; }
+        auto await_resume() noexcept {}
+        auto await_suspend(std::coroutine_handle<promise_type> handle) noexcept {
+          std::cout << "parent suspend" << std::endl;
+          auto parent = handle.promise().parent;
+          return parent ? parent : std::noop_coroutine();
+        }
+      };
+      std::cout << "final" << std::endl;
+      return Awaitable{};
+    }
+
     auto initial_suspend() -> std::suspend_never { std::cout << "initial" << std::endl; return {}; }
     auto return_value(Value value) -> void { std::cout << "return" << std::endl; this->value = value; }
     auto unhandled_exception() -> void {}
@@ -38,9 +52,13 @@ struct Task {
 
   auto await_ready() -> bool { std::cout << "task ready?" << std::endl; return handle.done(); }
 
-  auto await_suspend(std::coroutine_handle<> handle) {
+  auto await_resume() {
+    std::cout << "task resume" << std::endl;
+  }
+
+  auto await_suspend(std::coroutine_handle<> parent) {
     std::cout << "task suspend" << std::endl;
-    // this->handle.promise().set
+    handle.promise().parent = parent;
   }
 
   std::coroutine_handle<promise_type> handle;
