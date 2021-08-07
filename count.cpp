b3153e9933a6334a82337887d4372c8efdb64ca0
Tom <tom@contextfree.info>
2021-08-07 12:50:49 -0700

    Clean cpp reporting

--- count.cpp
+++ count.cpp
@@ -8,30 +8,36 @@
 
 // namespace kuro = exec;
 
-auto thread_id() { return std::this_thread::get_id(); }
+template<typename... Values>
+auto report(Values... values) {
+  std::cout << std::this_thread::get_id();
+  (std::ostream*[]) {&(std::cout << " " << values)...};
+  std::cout << std::endl;
+}
 
 auto count(size_t n, double interval) -> kuro::task<double> {
-  std::cout << thread_id() << " before loop " << interval << std::endl;
+  report("before loop", interval);
   auto start = std::chrono::high_resolution_clock::now();
   for (size_t i = 0; i < n; i += 1) {
     co_await kuro::sleep_for(interval);
-    std::cout << thread_id() << " slept " << interval << std::endl;
+    report("slept", interval);
   }
   auto elapsed = std::chrono::high_resolution_clock::now() - start;
   co_return elapsed.count() * 1e-9;
 }
 
 auto run() -> kuro::task<double> {
-  std::cout << thread_id() << " begin" << std::endl;
+  report("begin");
+  // std::cout << thread_id() << " begin" << std::endl;
   auto task1 = count(2, 1.0);
   auto task2 = count(3, 0.6);
-  std::cout << thread_id() << " count size: " << sizeof(task1) << std::endl;
+  report("count size:", sizeof(task1));
   auto [elapsed1, elapsed2] = co_await kuro::gather(task1, task2);
-  std::cout << thread_id() << " end" << std::endl;
+  report("end");
   co_return elapsed1 + elapsed2;
 }
 
 auto main() -> int {
   auto total = kuro::event_loop::run(run());
-  std::cout << thread_id() << " total: " << total << "\n";
+  report("total:", total);
 }
