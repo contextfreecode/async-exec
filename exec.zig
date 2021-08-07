a280b3cb5e89cc8a7c65f1dfb74be75f2a6a96c1
Tom <tom@contextfree.info>
2021-08-07 13:57:01 -0700

    Go back to timestamp

--- exec.zig
+++ exec.zig
@@ -5,12 +5,10 @@ const Task = struct { frame: anyframe, time: i128 };
 var task_list = [1]?Task{null} ** 10;
 var task_mutex = std.Thread.Mutex{};
 
-var timer = std.time.Timer.start();
-
 pub fn runLoop(endless: bool) void {
     while (true) {
         const lock = task_mutex.acquire();
-        const now = timer.read();
+        const now = std.time.nanoTimestamp();
         var any = false;
         var frame = for (task_list) |*task| {
             if (task.* != null) {
@@ -33,7 +31,7 @@ pub fn sleep(ns: u64) void {
         const lock = task_mutex.acquire();
         defer lock.release();
         const slot = findSlot();
-        const time = timer.read() + ns;
+        const time = std.time.nanoTimestamp() + ns;
         slot.* = Task{ .frame = @frame(), .time = time };
     }
 }
