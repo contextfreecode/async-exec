4df9d7d18dee2934c139821fd0a3457a857a1377
Tom <tom@contextfree.info>
2021-08-07 13:51:06 -0700

    Use timer for exec

--- exec.zig
+++ exec.zig
@@ -5,10 +5,12 @@ const Task = struct { frame: anyframe, time: i128 };
 var task_list = [1]?Task{null} ** 10;
 var task_mutex = std.Thread.Mutex{};
 
+var timer = std.time.Timer.start();
+
 pub fn runLoop(endless: bool) void {
     while (true) {
         const lock = task_mutex.acquire();
-        const now = std.time.nanoTimestamp();
+        const now = timer.read();
         var any = false;
         var frame = for (task_list) |*task| {
             if (task.* != null) {
@@ -31,7 +33,7 @@ pub fn sleep(ns: u64) void {
         const lock = task_mutex.acquire();
         defer lock.release();
         const slot = findSlot();
-        const time = std.time.nanoTimestamp() + ns;
+        const time = timer.read() + ns;
         slot.* = Task{ .frame = @frame(), .time = time };
     }
 }
